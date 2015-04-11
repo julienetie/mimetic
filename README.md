46d47fd24266fffa20c024f8d5cea2fa1045c2aa
Julien <Julienetie@gmail.com>
2015-04-11 20:43:58 +0100

    Set-up Gulp, organized file structure, re-bumped version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-#Mimetic...
+#Mimetic
 
+Mimetic considers a single width structure e.g,'1024' and scales the design to any screen size with minimal/ optional refinements necessary.
+
+Mimetic can work with any framework that can detatch from its grid system. Eg., Bootstrap, Foundation, etc.
+This current alpha release dosen't include Mimetic.stack, a feature to stack content for mobile devices.
+
+##Install
+
+Embed Mimetic directly into the page via < script > tags for seamless results, this is the recommended way.
+Place the script just before the </ body > tag.
+
+##Dependencies
+
+No dependencies, but to suport IE8 and below use <b>html5shiv</b> in the head.
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,88 @@
+var gulp          = require('gulp'),
+    jshint        = require('gulp-jshint'),
+    fileInsert    = require('gulp-file-insert'),
+    uglify        = require('gulp-uglify'),
+    rename        = require('gulp-rename'),
+    header        = require('gulp-header'),
+    pkg           = require('./package.json'),
 
-var gulp        = require('gulp'),
-    browserSync = require('browser-sync');
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync({
 
-        server: {
-            baseDir: "./"
-        }
-    });
+    js = './src/**/*.js',
+    sources = [
+    './src/**/*.js',
+    './src/**/*.html',
+    './src/**/*.css'],
+
+
+    banner = ['/**',
+  '/*',
+  ' *  <%= pkg.name %> - <%= pkg.description %>',
+  ' *    Version:  v<%= pkg.version %>',
+  ' *      License:  <%= pkg.license %>',
+  ' *        Author:  <%= pkg.author %>',
+  ' *          Contact:  julienetie@gmail.com',
+  ' *            github:  <%= pkg.repository.url %>',
+  ' *              twitter:  @julienetienne_',
+  ' *‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾',
+  ' *  Place/ inject Mimetic directly into the page before </ body>',
+  ' *  for seamless results .',
+  ' *',
+  ' */',
+  ''].join('\n'),
+
+
+    minBanner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @license <%= pkg.license %>',
+  ' * Paste or insert Mimetic directly into the page before </ body> for seamless results.',
+  ' */',
+  ''].join('\n');
+
+
+// ./dist/mimetic.min.js
+gulp.task('make-min', function() {
+  gulp.src(js)
+  .pipe(uglify())
+  .pipe(header(minBanner, { pkg : pkg } ))
+  .pipe(rename({
+    extname: '.min.js'
+  }))
+
+  .pipe(gulp.dest('./dist'));
 });
 
-// reloading browsers
-gulp.task('watch', browserSync.reload);
 
-gulp.task('default', ['browser-sync']);
 
-gulp.watch("html/*.html", ['watch']);
+
+// ./dist/mimetic.js
+gulp.task('make', function() {
+  gulp.src(js)
+  .pipe(header(banner, { pkg : pkg } ))  
+  .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('insert', function() {
+  gulp.src('./src/insert-template/mimetic.html').pipe(fileInsert({
+    '/* normalize.min.css */': './vendor/normalize.min.css',
+    '/* style.css */': './src/style.css',
+    '/* mimetic.js */': './src/mimetic.js',
+    '/* html5shiv.min.js */': './vendor/html5shiv.min.js'
+  })).pipe(gulp.dest('./'));
+});
+
+
+gulp.task('lint', function() {
+  return gulp.src(js)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(sources,['insert','lint','make-min','make']);
+});
+
+
+gulp.task('default', ['watch','lint','insert','make-min','make']);
