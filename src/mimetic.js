a647744669e881fc336a6d6cf2c1129462717e4d
Julien Etienne <j8@protonmail.ch>
2016-08-03 02:07:26 +0100

    Refactored unitsToREM conversion

--- src/mimetic.js
+++ src/mimetic.js
@@ -1,213 +1,236 @@
 (function(window, document, undefined) {
-  'use strict';
+    'use strict';
 
-  function mimeticFn() {
-
-    var options = {}; // **Only the first two are working on this release**
-
-    // Design to this width
-    options.designWidth = 1024;
-    // Add tags to exclude here
-    options.excludeTags = ['script', 'canvas'];
-    // Scale font-size
-    options.mimeticFonts = true;
-    // Scale line-height 
-    options.mimeticLineHt = true;
-    // Scale margin 
-    options.mimeticMargin = true;
-    // Scale padding  
-    options.mimeticPadding = true;
-    // For designs that exceed the overflow of the initial contiaining block.
-    options.infinateCanvas = false;
-
-    var
-      i,
-      pxVal,
-      designWidthRatio,
-      tempArr = [],
-      cleanArr = [],
-      fontSizeArr = [],
-      lineHtArr = [],
-      marginArr = [],
-      paddingArr = [],
-      tmpMgVal = [],
-      tmpPdVal = [];
+    var CSSUnits = [{
+      unit: 'px',
+      //  1 / 16
+      REMFactor: 0.0625
+    }, {
+      unit: 'pt',
+      // 96 / 72 / 16
+      REMFactor: 0.08333333333
+    }, {
+      unit: 'em',
+      // 1
+      REMFactor: 1
+    }, {
+      unit: 'rem',
+      // 1
+      REMFactor: 1
+    }];
 
 
-    var allElements = document.getElementsByTagName('body')[0].getElementsByTagName('*');
+    var CSSFixedUnits = [{
+      unit: 'xx-small',
+      // 9 / 16
+      REMValue: 0.5625
+    }, {
+      unit: 'x-small',
+      // 10 / 16
+      REMValue: 0.625
+    }, {
+      unit: 'small',
+      // 13 / 16
+      REMValue: 0.8125
+    }, {
+      unit: 'normal',
+      // 16 / 16
+      REMValue: 1
+    }, {
+      unit: 'medium',
+      // 16 / 16
+      REMValue: 1
+    }, {
+      unit: 'large',
+      // 18 / 16
+      REMValue: 1.125
+    }, {
+      unit: 'x-large',
+      // 24 / 16
+      REMValue: 1.5
+    }, {
+      unit: 'xx-large',
+      // 32 / 16
+      REMValue: 2
+    }];
 
 
-    var util = {
-        computedStyle: function(element, property) {
+    function mimeticFn() {
+
+      var options = {}; // **Only the first two are working on this release**
+
+      // Design to this width
+      options.designWidth = 1024;
+      // Add tags to exclude here
+      options.excludeTags = ['script', 'canvas'];
+      // Scale font-size
+      options.mimeticFonts = true;
+      // Scale line-height 
+      options.mimeticLineHt = true;
+      // Scale margin 
+      options.mimeticMargin = true;
+      // Scale padding  
+      options.mimeticPadding = true;
+      // For designs that exceed the overflow of the initial contiaining block.
+      options.infinateCanvas = false;
+
+      var
+        i,
+        designWidthRatio,
+        tempArr = [],
+        cleanArr = [],
+        fontSizeArr = [],
+        lineHtArr = [],
+        marginArr = [],
+        paddingArr = [],
+        tmpMgVal = [],
+        tmpPdVal = [];
+
+
+      var allElements = document.getElementsByTagName('body')[0].getElementsByTagName('*');
+
+
+      function aliasValueToREM(value) {
+
+
+        return CSSFixedUnits.filter(function(metricInfo) {
+          return value === metricInfo.unit;
+        }).shift().REMValue;
+      }
+
+
+      function unitsToREM(value) {
+        var suffix = value.replace(/[^a-z]+/gi, '');
+        var numberValue = value.replace(/[^\d\.]*/g, '');
+        var remValue;
+
+        var metricInfo = CSSUnits.filter(function(metricInfo) {
+            return suffix === metricInfo.unit;
+          })
+          .shift();
+
+          remValue = metricInfo ? numberValue * metricInfo.REMFactor : aliasValueToREM(value);
+
+          return remValue;
+        }
+
+        function computedStyle(element, property) {
           return window.getComputedStyle(element, null).getPropertyValue(property);
-        },
-
-
-        unitsToREM: function(val) {
-          console.log(val)
-          if (val.indexOf('px') >= 0) {
-            pxVal = parseInt(val, 10);
-          } else if (val.indexOf('pt')  >= 0) {
-            pxVal = parseInt(val, 10) * 96 / 72;
-          } else if (val.indexOf('em')  >= 0) {
-            pxVal = parseInt(val, 10) * 16;
-          } else {
-
-            switch (val) {
-              case 'xx-small':
-                pxVal = 9;
-                break;
-              case 'x-small':
-                pxVal = 10;
-                break;
-              case 'small':
-                pxVal = 13;
-                break;
-              case 'normal':
-                pxVal = 16;
-                break;
-              case 'medium':
-                pxVal = 16;
-                break;
-              case 'large':
-                pxVal = 18;
-                break;
-              case 'x-large':
-                pxVal = 24;
-                break;
-              case 'xx-large':
-                pxVal = 32;
-                break;
-              case 'larger':
-                console.log('font-size: "larger" is not supported');
-                pxVal = 16;
-                break;
-              case 'smaller':
-                console.log('font-size: "smaller" is not supported');
-                pxVal = 16;
-                break;
-            }
-          }
-          return pxVal;
-
-        },
-
-
-        convertUnits: function(propDef, iter) {
-          var preSfx = propDef[iter].slice(-2),
-            cleanPropDef = parseInt(propDef[iter], 10),
-            suffix = {
-              'px': cleanPropDef,
-              'em': cleanPropDef * 16,
-              '%': 1024 / cleanPropDef,
-              'pt': cleanPropDef * 96 / 72,
-              cutSuffix: function(suffixType) {
-                propDef[iter] = this[suffixType];
-                return propDef;
-              }
-            }.cutSuffix(preSfx);
-        },
-
-
-        sanitizeShorthand: function(propVal) {
-          var propValDef = propVal.split(' ');
-          for (var i = 0; i < propValDef.length; i++) {
-            this.convertUnits(propValDef, i);
-          }
-          return propValDef;
-
-        },
-
-
-        setBoundaryProp: function(propType, tempPropVal, propArr, iterI) {
-          tempPropVal = [];
-          i = 0;
-          while (i < propArr[iterI].length) {
-            tempPropVal.push(Math.round(designWidthRatio * (propArr[iterI][i] / 10)) + 'px');
-            if (i === propArr[iterI].length - 1) {
-              allElements[iterI].style[propType] = tempPropVal.join(' ');
-            }
-            i++;
-          }
-        },
-      },
-
-
-      elemLoop = (function() {
-
-        var remove = options.excludeTags;
-        for (var i = 0; i < allElements.length; i++) {
-          tempArr.push(allElements[i].tagName);
-          for (var j = 0; j < remove.length; j++) {
-            if (tempArr[i] === remove[j].toUpperCase())
-              tempArr[i] = undefined;
-          }
-
-  // console.log('font-size', util.computedStyle(allElements[i], 'line-height'))
-
-          fontSizeArr.push(
-            util.unitsToREM(
-              util.computedStyle(
-                allElements[i], 'font-size')
-            )
-          );
-
-          lineHtArr.push(
-            util.unitsToREM(
-              util.computedStyle(
-                allElements[i], 'line-height')
-            )
-          );
-
-          marginArr.push(
-            util.sanitizeShorthand(
-              util.computedStyle(
-                allElements[i], 'margin')
-            )
-          );
-
-          paddingArr.push(
-            util.sanitizeShorthand(
-              util.computedStyle(
-                allElements[i], 'padding')
-            )
-          );
-
         }
 
-        cleanArr = tempArr;
+        var util = {
+            convertUnits: function(propDef, iter) {
+              var preSfx = propDef[iter].slice(-2),
+                cleanPropDef = parseInt(propDef[iter], 10),
+                suffix = {
+                  'px': cleanPropDef,
+                  'em': cleanPropDef * 16,
+                  '%': 1024 / cleanPropDef,
+                  'pt': cleanPropDef * 96 / 72,
+                  cutSuffix: function(suffixType) {
+                    propDef[iter] = this[suffixType];
+                    return propDef;
+                  }
+                }.cutSuffix(preSfx);
+            },
 
-      }()),
+
+            sanitizeShorthand: function(propVal) {
+              var propValDef = propVal.split(' ');
+              for (var i = 0; i < propValDef.length; i++) {
+                this.convertUnits(propValDef, i);
+              }
+              return propValDef;
+
+            },
 
 
-      mimetic = {
-        scale: function() {
+            setBoundaryProp: function(propType, tempPropVal, propArr, iterI) {
+              tempPropVal = [];
+              i = 0;
+              while (i < propArr[iterI].length) {
+                tempPropVal.push(Math.round(designWidthRatio * (propArr[iterI][i] / 10)) + 'px');
+                if (i === propArr[iterI].length - 1) {
+                  allElements[iterI].style[propType] = tempPropVal.join(' ');
+                }
+                i++;
+              }
+            },
+          },
 
-          var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-          if (winWidth > 1024) {
 
-            designWidthRatio = (winWidth / (options.designWidth * 0.1));
-            for (var i = 0; i < cleanArr.length; i++) {
+          elemLoop = (function() {
 
-              if (tempArr[i] !== undefined) {
-                allElements[i].style.fontSize = Math.round(designWidthRatio * (fontSizeArr[i] / 10)) + 'px';
-                allElements[i].style.lineHeight = Math.round(designWidthRatio * (lineHtArr[i] / 10)) + 'px';
-                util.setBoundaryProp('margin', tmpMgVal, marginArr, i);
-                util.setBoundaryProp('padding', tmpPdVal, paddingArr, i);
+            var remove = options.excludeTags;
+            for (var i = 0; i < allElements.length; i++) {
+              tempArr.push(allElements[i].tagName);
+              for (var j = 0; j < remove.length; j++) {
+                if (tempArr[i] === remove[j].toUpperCase())
+                  tempArr[i] = undefined;
+              }
 
-              } else {
-                // 
+              // console.log('font-size', computedStyle(allElements[i], 'line-height'))
+
+              fontSizeArr.push(
+                unitsToREM(
+                  computedStyle(
+                    allElements[i], 'font-size')
+                )
+              );
+
+              lineHtArr.push(
+                unitsToREM(
+                  computedStyle(
+                    allElements[i], 'line-height')
+                )
+              );
+
+              marginArr.push(
+                util.sanitizeShorthand(
+                  computedStyle(
+                    allElements[i], 'margin')
+                )
+              );
+
+              paddingArr.push(
+                util.sanitizeShorthand(
+                  computedStyle(
+                    allElements[i], 'padding')
+                )
+              );
+
+            }
+
+            cleanArr = tempArr;
+
+          }()),
+
+
+          mimetic = {
+            scale: function() {
+              var winWidth = window.innerWidth;
+              if (winWidth > 1024) {
+
+                designWidthRatio = winWidth / options.designWidth * 0.1;
+                for (var i = 0; i < cleanArr.length; i++) {
+
+                  if (tempArr[i] !== undefined) {
+                    allElements[i].style.fontSize = Math.round(designWidthRatio * (fontSizeArr[i] / 10)) + 'rem';
+                    allElements[i].style.lineHeight = Math.round(designWidthRatio * (lineHtArr[i] / 10)) + 'rem';
+                    util.setBoundaryProp('margin', tmpMgVal, marginArr, i);
+                    util.setBoundaryProp('padding', tmpPdVal, paddingArr, i);
+
+                  }
+                }
               }
             }
-          }
-        }
-      };
+          };
 
-    mimetic.scale();
-    resizilla(mimetic.scale, 180, false);
-  }
+        mimetic.scale();
+        resizilla(mimetic.scale, 180, false);
+      }
 
-  window.addEventListener("load", function(){
-      mimeticFn();
-  });
-}(window, document, undefined));
+      window.addEventListener("load", function() {
+        mimeticFn();
+      });
+    }(window, document, undefined));
