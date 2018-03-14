170faf772a7c8ae93e3ee5033db6cb216f117140
Julien Etienne <j8@protonmail.ch>
2018-03-14 01:56:56 +0000

    Created basic browser automation test via wdio

--- test/browser-automation/index.js
+++ test/browser-automation/index.js
@@ -1,26 +1,86 @@
-const webdriverio = require('webdriverio');
-const options = { desiredCapabilities: { browserName: 'chrome' } };
-const client = webdriverio.remote(options);
+const fs = require('fs');
+const http = require('http');
+const PORT = 1234
+const resizeDelay = 400;
+const serve = require('serve');
+const killPort = require('kill-port');
+// fs.readFile('./test/browser-automation/index.html', function(err, html) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     http.createServer((req, res) => {
+//         res.writeHeader(200, { "Content-Type": "text/html" });
+//         res.write(html);
+//         res.end();
+//     }).listen(PORT);
+// });
 
-// Basic automation without tests setup.
-client
-    .init()
-    .url('https://duckduckgo.com')
-    .windowHandleSize({ width: 400, height: 400 })
-    .pause(1000)
-    .windowHandleSize({ width: 700, height: 500 })
-    .pause(1000)
-    .windowHandleSize({ width: 800, height: 600 })
-    .pause(1000)
-    .windowHandleSize({ width: 1300, height: 800 })
-    .pause(1000)
-    .windowHandleSize({ width: 1400, height: 900 })
-    .pause(1000)
 
-    .setValue('#search_form_input_homepage', 'github MIMETIC')
-    .click('#search_button_homepage')
-    .getTitle()
-    .then(function(title) {
-        console.log('Title is: ' + title);
-    })
-    .end();
+// const server = serve(__dirname, {
+//     port: PORT,
+//     ignore: ['node_modules']
+// })
+
+
+
+describe('MIMETIC', () => {
+    it('Title should be MIMETIC Browser Automation', () => {
+        browser.url('/test/browser-automation/index.html')
+        const title = browser.getTitle();
+        expect(title).to.equal('MIMETIC Browser Automation');
+    });
+
+    it('Window should be greater than 1024 * 768', () => {
+        browser
+            .windowHandleSize({ width: 400, height: 400 })
+            .pause(resizeDelay)
+
+        browser
+            .windowHandleSize({ width: 700, height: 500 })
+            .pause(resizeDelay)
+
+
+        browser
+            .windowHandleSize({ width: 800, height: 600 })
+            .pause(resizeDelay)
+
+        browser
+            .windowHandleSize({ width: 1300, height: 800 })
+            .pause(resizeDelay)
+
+        browser
+            .windowHandleSize({ width: 1400, height: 900 })
+            .pause(resizeDelay)
+
+        browser
+            .then(function(title) {
+                killPort(PORT)
+                    // .then(() => {
+                        // expect(true).to.equal(true);
+                        browser.end();
+                    // })
+            });
+
+
+    });
+});
+
+
+
+// client
+//     .init()
+//     .url(`http://localhost:${PORT}`)
+//     .windowHandleSize({ width: 400, height: 400 })
+//     .pause(1000)
+//     .windowHandleSize({ width: 700, height: 500 })
+//     .pause(1000)
+//     .windowHandleSize({ width: 800, height: 600 })
+//     .pause(1000)
+//     .windowHandleSize({ width: 1300, height: 800 })
+//     .pause(1000)
+//     .windowHandleSize({ width: 1400, height: 900 })
+//     .pause(1000)
+//     .then(function(title) {
+//         client.end();
+//     })
+//
