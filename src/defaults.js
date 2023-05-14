5a2ee4685eb34f43d8163cbff3641661135d76f7
Julien Etienne <j8@envidia.io>
2023-05-14 23:42:52 +0100

    Cleaned up defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,17 +1,46 @@
-/**
- * Default config properties if not defined.
- */
+/*
+Default config properties if not defined 
+*/
 const defaults = {
-  rootSelector: 'html', // The element used to manipulate em and rem units
-  memisisBreakpoint: '64em', // The viewport width breakpoint to begin scaling
-  scale: true, // Enable/ disable scaling
-  loadEvent: 'DOMContentLoaded', // Load type
-  resizeDelay: 20, // Debounce delay between calls on resize.
-  calc: null, // Maniputate the font value manually using a function.
-  relativeDesignWidth: 1024, // The width relative to the font size.
-  onScale: null, // Callback triggered on scale
-  onZoom: null, // Callback triggered on zoom
-  onResize: null // Callback triggered on window resize
+  /* 
+  The element used to manipulate em and rem units */
+  rootSelector: 'html',
+
+  /* 
+  The viewport width breakpoint to begin scaling */
+  memisisBreakpoint: '64em',
+
+  /* 
+  Enable/ disable scaling */
+  scale: true,
+
+  /* 
+  Load type */
+  loadEvent: 'DOMContentLoaded',
+
+  /* 
+  Debounce delay between calls on resize */
+  resizeDelay: 20,
+
+  /* 
+  Maniputate the font value manually using a function */
+  calc: null,
+  
+  /* 
+  The width relative to the font size */
+  relativeDesignWidth: 1024,
+
+  /*
+  Callback triggered on scale */
+  onScale: null,
+
+  /* 
+  Callback triggered on zoom */
+  onZoom: null, 
+
+  /* 
+  Callback triggered on window resize */
+  onResize: null, 
 }
 
 export default defaults
